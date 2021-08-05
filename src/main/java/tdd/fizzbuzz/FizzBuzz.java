706810b927c956bbb362150b15ab9f69f0108831
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-05 14:02:37 +0800

    [FizzBuzz][ZULUEJA] feat: implemented multiple of 5 function

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     public String countOff(int orderNumber){
         if(orderNumber % 3 ==0){
             return "Fizz";
+        } else if(orderNumber % 5 == 0){
+            return "Buzz";
         }
         return String.valueOf(orderNumber);
     }
